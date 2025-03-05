971ebf5f8f5c2fd04850fbf1701ae934c32f5a4b
luchian <luchian-1999@mail.ru>
2025-03-05 18:10:53 +0300

    partition and sorting in one single function leads to maximum deapth error

---
+++ quick_sort.py
@@ -0,0 +1,24 @@
+import random as rd 
+
+
+
+def QuickSort(A,start,end):
+	#to sort entire A is to sort A from 0 to len(A)-1
+	if start < end:
+		p = rd.randint(start,end)
+		A[end],A[p] = A[p],A[end]
+		p_el = A[end]
+		p = start
+		for ind in range(start,end):
+			if A[ind] <= p_el:
+				A[p],A[ind] = A[ind],A[p]
+				p += 1
+		A[end],A[p] = A[p],A[end]
+		QuickSort(A,start,p-1)
+		QuickSort(A,p+1,end)
+	return A
+
+some_ar = [rd.randint(1,101) for _ in range(100000)]
+print(some_ar)
+QuickSort(some_ar,0,len(some_ar)-1)
+print(some_ar)
